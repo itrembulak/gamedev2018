13fc3ad3758740d0f28cadac9c1b0f8d9d1bf9ae
František Ferenčík <frantisek.ferencik@student.tuke.sk>
2018-03-16 14:14:59 +0100

    bradle fix

--- core/src/sk/feri/awesomegame/Projectile.java
+++ core/src/sk/feri/awesomegame/Projectile.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.Screen;
 public class Projectile extends DynamicGameObject {
 	public static final float PROJCTILE_WIDTH = 1;
 	public static final float PROJCTILE_HEIGHT = 0.6f;
-	public static final float PROJCTILE_VELOCITY = 30f;
+	public static final float PROJCTILE_VELOCITY = 3f;
 	public static final float PROJCTILE_RANGE = 17f;
 	public static final int PROJECTILE_STATE_FIRED = 0;
 	public static final int PROJECTILE_STATE_DESTROY = 1;
@@ -30,7 +30,7 @@ public class Projectile extends DynamicGameObject {
 		bounds.y = position.y - PROJCTILE_HEIGHT / 2;
 
 		if (state != PROJECTILE_STATE_DESTROY)
-			velocity.y = + PROJCTILE_VELOCITY;
+			velocity.y += PROJCTILE_VELOCITY;
 		velocity.x = 0;
 
 		if (position.y - fired_y > PROJCTILE_RANGE){
